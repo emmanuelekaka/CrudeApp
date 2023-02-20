996929dcb42d907dc8b57693adc157adc037d7ea
emma_frost <ekakashean@gmail.com>
2023-02-20 12:13:55 +0300

    chore: reverted code from es6 to cjs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // Adding express to the project
-import express from 'express';
+
+const express = require ('express');
 const app = express();
 
 // initializing express
+// app.get('/',(req,res)=>{
+//     res.send('<h1>Hello World</h1>');
+// })
+// app.get('/about',(req,res)=>{
+//     res.send('<h1>About us</h1>');
+// })
+
+// Dealing with html files
 app.get('/',(req,res)=>{
-    res.send('<h1>Hello World</h1>');
+    res.sendFile('./views/index.html',{root:__dirname});
 })
 app.get('/about',(req,res)=>{
-    res.send('<h1>About us</h1>');
+    res.sendFile('./views/about.html',{root:__dirname});
 })
 
 
